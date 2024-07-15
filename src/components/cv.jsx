7e15379084625a6f8b8db38d9ddb950232a34081
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-07-15 16:32:07 +0200

    Added formatting to files

--- src/components/cv.jsx
+++ src/components/cv.jsx
@@ -1,5 +1,5 @@
 export default function CV() {
-	return(
+	return (
 		<main></main>
 	)
 }
--- src/components/form/formSection.jsx
+++ src/components/form/formSection.jsx
@@ -3,15 +3,15 @@ export default function FormSection({ information }) {
 		<div>
 			{information.map((element) =>
 				element.label !== "description" ?
-				<>
-					<label htmlFor={element.label}>{element.content}</label>
-					<input type={element.input} placeholder={element.content} id={element.label} />
-				</>
-				:
-				<>
-					<label htmlFor={element.label}>{element.content}</label>
-					<textarea type={element.input} placeholder={element.content} id={element.label} />
-				</>
+					<>
+						<label htmlFor={element.label}>{element.content}</label>
+						<input type={element.input} placeholder={element.content} id={element.label} />
+					</>
+					:
+					<>
+						<label htmlFor={element.label}>{element.content}</label>
+						<textarea type={element.input} placeholder={element.content} id={element.label} />
+					</>
 			)}
 		</div>
 	)
