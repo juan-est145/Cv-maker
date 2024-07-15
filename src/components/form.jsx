696d412e2d8cd7ae55f72c39854e10065ed85ecb
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-07-15 12:32:11 +0200

    Added form component

---
+++ src/components/form.jsx
@@ -0,0 +1,9 @@
+import GeneralInfo from "./generalInfo"
+
+export default function Form() {
+	return (
+		<aside>
+			<GeneralInfo></GeneralInfo>
+		</aside>
+	)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GeneralInfo from './components/generalInfo'
+import Form from './components/form'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GeneralInfo></GeneralInfo>
+    <Form></Form>
   </React.StrictMode>,
 )
