11af3568994a2fb97408d39c5912a6483b801f1d
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-07-15 13:34:42 +0200

    Added subdirectory to form section

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Form from './components/form'
+import Form from './components/form/form'
+import CV from './components/cv'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Form></Form>
+    <CV></CV>
   </React.StrictMode>,
 )
