346dc06d6dd08d95985e4064da8e977b5f10b424
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-07-15 12:22:20 +0200

    Added object for tags for better usability

--- src/components/generalInfo.jsx
+++ src/components/generalInfo.jsx
@@ -1,20 +1,34 @@
 export default function GeneralInfo() {
+	const labels = {
+		firstName : "first-name",
+		lastName: "last-name",
+		description: "description",
+		telephone: "telephone",
+		email: "email",
+		linkedin: "linkedin"
+	};
+
 	return (
 		<>
 		<h3>General information</h3>
 		<form action="post">
 			<fieldset>
 			<div>
-				<label htmlFor="first-name">First name</label>
-				<input type="text" placeholder="First name" id="first-name"/>
-				<label htmlFor="last-name">Last name</label>
-				<input type="text" placeholder="Last name" id="last-name"/>
+				<label htmlFor={labels.firstName}>First name</label>
+				<input type="text" placeholder="First name" id={labels.firstName}/>
+				<label htmlFor={labels.lastName}>Last name</label>
+				<input type="text" placeholder="Last name" id={labels.lastName}/>
 			</div>
-			
-				
-			<input type="text" placeholder="Description"/>
-			<input type="tel" placeholder="Telephone"/>
-			<input type="" />
+			<div>
+				<label htmlFor={labels.description}>Description</label>
+				<input type="text" placeholder="Description" id={labels.description}/>
+				<label htmlFor={labels.telephone}>Telephone</label>
+				<input type="tel" placeholder="Telephone" id={labels.telephone}/>
+				<label htmlFor={labels.email}>Email</label>
+				<input type="email" placeholder="Email" id={labels.email}/>
+				<label htmlFor={labels.email}>Linkedin</label>
+				<input type="url" placeholder="Linkedin" id={labels.email}/>
+			</div>
 			</fieldset>
 		</form>
 		</>
