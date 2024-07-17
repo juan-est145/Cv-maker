3ba6a6678872e26cffb74f7dd287b8b08180f21a
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-07-17 18:12:14 +0200

    Cv component now shows all properties and does so automatically

--- src/components/cv.jsx
+++ src/components/cv.jsx
@@ -1,20 +1,21 @@
 export default function CV({ genInfoState, eduState, workState }) {
 	return (
 		<main>
-			<div>
-				<h1>{genInfoState.firstName}</h1>
-				<h1>{genInfoState.description}</h1>
-				<h1>{genInfoState.linkedin}</h1>
-				<h1>{genInfoState.telephone}</h1>
-			</div>
-			<div>
-				<h1>{eduState.studyTitle}</h1>
-				<h1>{eduState.descriptionTitle}</h1>
-				<h1>{eduState.startDate}</h1>
-			</div>
-			<div>
-				<h1>{workState.companyName}</h1>
-			</div>
+			<section>
+				{Object.values(genInfoState).map((element, index) => {
+					return <h1 key={index}>{element}</h1>;
+				})}
+			</section>
+			<section>
+				{Object.values(eduState).map((element, index) => {
+					return <h1 key={index}>{element}</h1>;
+				})}
+			</section>
+			<section>
+				{Object.values(workState).map((element, index) => {
+					return <h1 key={index}>{element}</h1>;
+				})}
+			</section>
 		</main>
 	)
 }
