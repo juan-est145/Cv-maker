11bda9213ca8a41459d0fd9aa0d553235a39ca63
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-07-21 09:22:53 +0200

    Education button is created, need to style it

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -3,6 +3,7 @@ import "../../styles/form.css"
 
 let confirmBtnClass = "confirmBtn";
 let resetBtnClass = "resetBtn";
+let addSectionBtn = "addBtn";
 
 export default function Form({
 	genInfoState,
@@ -116,6 +117,7 @@ function Education({ eduState, setEdu }) {
 						<button className={resetBtnClass} onClick={resetBtn}>Reset</button>
 						<button className={confirmBtnClass} onClick={showBtn}>Confirm</button>
 					</div>
+					<button className={addSectionBtn} >Add new education</button>
 				</fieldset>
 			</form>
 		</>
